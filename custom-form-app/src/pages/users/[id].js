d2580a68736b807de015a6e0f79a9e2a5d6101de
halimdirgantara <halimdirgantara@gmail.com>
2023-02-24 05:56:44 +0700

    not fix! fecth data and show in form

--- custom-form-app/src/pages/users/[id].js
+++ custom-form-app/src/pages/users/[id].js
@@ -2,17 +2,19 @@ import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
 import useSWR from 'swr'
 import axios from '@/lib/axios'
-import ButtonEdit from '@/components/ButtonEdit'
-import Link from 'next/link'
+// import ButtonEdit from '@/components/ButtonEdit'
+// import Link from 'next/link'
 import Input from '@/components/Input'
 import InputError from '@/components/InputError'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 const UserPage = () => {
     const router = useRouter()
     const { id } = router.query
-    const { data: userpage, error } = useSWR(`/api/user/${id}`, () =>
+
+    const { data: userdata, error } = useSWR(`/api/user/${id}`, () =>
         axios
             .get(`/api/user/${id}`)
             .then(res => res.data)
@@ -20,11 +22,36 @@ const UserPage = () => {
                 if (error.response.status !== 409) throw error
             }),
     )
-    const [name, setName] = useState('')
+    if (!userdata) {
+        return <div>Loading</div>
+    }
+
+    console.log(userdata)
+    const [name, setName] = useState(userdata?.name)
     const [email, setEmail] = useState('')
+    const [role, setRole] = useState('')
+    const [status, setStatus] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
-    const [errors, setErrors] = useState([])
+    const [errors, setErrors] = useState({})
+    const [isDisabled, setIsDisabled] = useState(true)
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        // Handle form submission here
+        // name,
+        // email,
+        // role,
+        // status,
+        // password,
+        // password_confirmation: passwordConfirmation,
+        // setErrors,
+    }
+
+    const handleEditButtonClick = () => {
+        setIsDisabled(!isDisabled)
+    }
+
     return (
         <AppLayout
             header={
@@ -32,103 +59,157 @@ const UserPage = () => {
                     User
                 </h2>
             }>
-
             <Head>
-                <title>Custom Form - </title>
+                <title>Custom Form - {name}</title>
             </Head>
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
                             <div className="relative overflow-x-auto">
-                                <form>
-                                    <div className='mb-6'>
-                                        <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-6">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Name
+                                        </label>
                                         <Input
                                             id="name"
                                             type="text"
-                                            value={userpage?.name}
+                                            value={name}
                                             className="block mt-1 w-full"
-                                            onChange={event => setName(event.target.value)}
+                                            onChange={event =>
+                                                setName(event.target.value)
+                                            }
                                             required
-                                            disabled
-                                            autoFocus
+                                            disabled={isDisabled}
                                         />
-                                        <InputError messages={errors.name} className="mt-2" />
+                                        <InputError
+                                            messages={errors.name}
+                                            className="mt-2"
+                                        />
                                     </div>
-                                    <div className='mb-6'>
-                                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <div className="mb-6">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Email
+                                        </label>
                                         <Input
                                             id="email"
                                             type="text"
-                                            value={userpage?.email}
+                                            value={email}
                                             className="block mt-1 w-full"
-                                            onChange={event => setEmail(event.target.value)}
+                                            onChange={event =>
+                                                setEmail(event.target.value)
+                                            }
                                             required
-                                            disabled
-                                            autoFocus
+                                            disabled={isDisabled}
                                         />
-                                        <InputError messages={errors.name} className="mt-2" />
+                                        <InputError
+                                            messages={errors.email}
+                                            className="mt-2"
+                                        />
                                     </div>
-                                    <div className='mb-6'>
-                                        <label for="role" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                                    <div className="mb-6">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Role
+                                        </label>
                                         <Input
                                             id="role"
                                             type="text"
-                                            value={userpage?.role}
+                                            value={role}
                                             className="block mt-1 w-full"
-                                            onChange={event => setRole(event.target.value)}
-                                            disabled
+                                            onChange={event =>
+                                                setRole(event.target.value)
+                                            }
+                                            disabled={isDisabled}
                                             required
-                                            autoFocus
                                         />
-                                        <InputError messages={errors.name} className="mt-2" />
+                                        <InputError
+                                            messages={errors.role}
+                                            className="mt-2"
+                                        />
                                     </div>
-                                    <div className='mb-6'>
-                                        <label for="status" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+                                    <div className="mb-6">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Status
+                                        </label>
                                         <Input
                                             id="status"
                                             type="text"
-                                            value={userpage?.status}
+                                            value={status}
                                             className="block mt-1 w-full"
-                                            onChange={event => setStatus(event.target.value)}
+                                            onChange={event =>
+                                                setStatus(event.target.value)
+                                            }
                                             required
-                                            disabled
-                                            autoFocus
+                                            disabled={isDisabled}
                                         />
-                                        <InputError messages={errors.name} className="mt-2" />
+                                        <InputError
+                                            messages={errors.status}
+                                            className="mt-2"
+                                        />
                                     </div>
-                                    <div className='mb-6'>
-                                        <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <div className="mb-6">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Password
+                                        </label>
                                         <Input
                                             id="password"
                                             type="password"
-                                            value='********'
+                                            value={password}
                                             className="block mt-1 w-full"
-                                            onChange={event => setPassword(event.target.value)}
-                                            autoFocus
+                                            onChange={event =>
+                                                setPassword(event.target.value)
+                                            }
+                                            disabled={isDisabled}
                                         />
-                                        <InputError messages={errors.name} className="mt-2" />
+                                        <InputError
+                                            messages={errors.password}
+                                            className="mt-2"
+                                        />
                                     </div>
-                                    <div className='mb-6'>
-                                        <label for="password_confirmation" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password Confirmation</label>
+                                    <div className="mb-6">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Password Confirmation
+                                        </label>
                                         <Input
                                             id="password_confirmation"
                                             type="password"
-                                            value='********'
+                                            value={passwordConfirmation}
                                             className="block mt-1 w-full"
-                                            onChange={event => setPasswordConfirmation(event.target.value)}
-                                            autoFocus
+                                            onChange={event =>
+                                                setPasswordConfirmation(
+                                                    event.target.value,
+                                                )
+                                            }
+                                            disabled={isDisabled}
                                         />
-                                        <InputError messages={errors.name} className="mt-2" />
+                                        <InputError
+                                            messages={
+                                                errors.passwordConfirmation
+                                            }
+                                            className="mt-2"
+                                        />
                                     </div>
-                                    <button
-                                        id="edit-button"
-                                        className="edit-button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                    >Edit</button>
+                                    <div className="flex justify-end">
+                                        <button
+                                            type="button"
+                                            className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                                            onClick={handleEditButtonClick}>
+                                            {isDisabled ? 'Edit' : 'Cancel'}
+                                        </button>
+                                        <button
+                                            type="submit"
+                                            className={`ml-4 px-4 py-2 text-sm font-medium text-white rounded-md ${
+                                                isDisabled
+                                                    ? 'bg-gray-500 cursor-not-allowed'
+                                                    : 'bg-green-500 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2'
+                                            }`}
+                                            disabled={isDisabled}>
+                                            Save
+                                        </button>
+                                    </div>
                                 </form>
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -136,5 +217,4 @@ const UserPage = () => {
         </AppLayout>
     )
 }
-
 export default UserPage
